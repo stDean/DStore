cbfe258ad3731a02ebcf9b4c2a239404edb6e557
Toba-dean <tobaogundimu@gmail.com>
2023-10-28 06:51:23 +0100

    added logic for sorting, filtering and paginating the products

--- server/controller/product.ctrl.js
+++ server/controller/product.ctrl.js
@@ -1,4 +1,4 @@
-const { NotFoundError } = require("../errors");
+const { NotFoundError, BadRequestError } = require("../errors");
 const Product = require("../model/product.schema");
 const { StatusCodes } = require("http-status-codes");
 const slugify = require("slugify");
@@ -14,7 +14,50 @@ const ProductCtrl = {
     res.status(StatusCodes.CREATED).json(product);
   },
   getProducts: async (req, res) => {
-    const products = await Product.find({});
+    // filtering
+    let queryObject = { ...req.query };
+    const excludeList = ["page", "limit", "sort", "fields"];
+    excludeList.forEach(item => {
+      delete queryObject[item];
+    });
+
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(
+      /\b(gt|gte|eq|lt|lte)\b/g,
+      match => `$${match}`
+    );
+
+    let result = Product.find(JSON.parse(queryStr));
+
+    // sorting
+    if (req.query.sort) {
+      // split the string then join by a space not comma
+      const sortList = req.query.sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createdAt");
+    }
+
+    // limiting the field to return in the query
+    if (req.query.fields) {
+      const fieldsList = req.query.fields.split(",").join(" ");
+      result = result.select(fieldsList);
+    }
+
+    // Limiting and pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    if (page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) {
+        throw new BadRequestError("Page does not exist.");
+      }
+    }
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
     res.status(StatusCodes.OK).json({ products, nbHits: products.length });
   },
   getProduct: async (req, res) => {
